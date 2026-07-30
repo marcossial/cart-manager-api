7d650d3ac3149e1e67018c6593fd897a61670f9f
Marcos Silva Almeida <marcossial043@gmail.com>
2026-07-30 07:47:14 -0300

    fix: Permissão de GET para usuarios comuns

--- src/main/java/com/marcossial/cartmanager/config/SecurityConfig.java
+++ src/main/java/com/marcossial/cartmanager/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.jspecify.annotations.Nullable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -107,6 +108,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(ENDPOINTS_SEM_AUTENTICACAO).permitAll()
                         .requestMatchers(ENDPOINTS_COM_AUTENTICACAO).authenticated()
+                        .requestMatchers(HttpMethod.GET, ENDPOINTS_ADMIN).hasAnyRole("ADMIN", "USUARIO")
                         .requestMatchers(ENDPOINTS_ADMIN).hasRole("ADMIN")
                         .requestMatchers(ENDPOINTS_USUARIO).hasAnyRole("ADMIN", "USUARIO")
                         .anyRequest().denyAll()
